963145fe06910adc91de21132825f144e8d06cf0
hsulli5 <35343448+hsulli5@users.noreply.github.com>
2018-02-20 11:13:44 -0600

    code 3

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -1,28 +1,15 @@
 // assign values to a set of variables representing a project's metadata
 let projectId = 1
-let projectUrl = 'https://www.arcgis.com/apps/View/index.html?appid=9f509aae7b95477e8008f3cabff244a6'
-let projectTitle = 'Map of Earthquakes'
+let projectUrl = 'http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d'
+let projectTitle = 'Map of Recent Earthquakes'
 let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.'
 let projectThumb = true
 let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
 
-let projectArray = [
-  1,
-  'https://www.arcgis.com/apps/View/index.html?appid=9f509aae7b95477e8008f3cabff244a6',
-  'Map of Earthquakes',
-  'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS',
-  true,
-  ['earthquakes', 'usgs']
-]
-
-let projectObject = {
-  id:1,
-  url: 'https://www.arcgis.com/apps/View/index.html?appid=9f509aae7b95477e8008f3cabff244a6',
-  title: 'Map of Earthquakes',
-  description: 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS',
-  hasThumbnail: true
-  keywords:['earthquakes', 'usgs']
-}
-
 // display messages in the browser's developer console, combining the values with text
-console.log(projectObject)
+console.log('ID: ' + 1)
+console.log('URL: ' + http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d )
+console.log('Title: ' + Map of Recent Earthquakes)
+console.log('Description: ' + An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS)
+console.log('Has thumbnail? ' + true)
+console.log('Map extent: ' + [48.51, -28.37, 2.63, -132.34])
