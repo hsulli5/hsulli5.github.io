38b275874cad4944916d53f74ba63e826198dece
hsulli5 <35343448+hsulli5@users.noreply.github.com>
2018-02-22 11:49:50 -0600

    code 5

--- js/code-assignment-05.js
+++ js/code-assignment-05.js
@@ -1,22 +1,20 @@
-// create an object and assign six variables to property names (i.e., keys)
-let projectObject = {
+let projects = [{
   id: 1,
   url: "http://arcg.is/2FbRnLQ",
   title: 'Map of Austin, TX',
   desc: 'This is a story map of Austin, TX.',
   thumb: true,
   projectKeywords: ['Austin', 'story', 'map']
-}
-
-let projectObject = {
+},
+{
   id: 2,
   url: 'http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d',
   title: 'Earthquakes',
   desc: 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. Info pulled from USGS.',
   thumb: true,
   projectKeywords: ['USGS', 'Earthquakes']
+}]
+
+for (let i=0; i < projects.length; i++) {
+  console.log('The value of i is; ' +i)
 }
-
-let projects = [{project1}, {project2}]
-
-console.log(projects);
