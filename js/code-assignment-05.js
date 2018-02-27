da7d8c157b1108764f1ad556c3c83695c7e77c05
hsulli5 <35343448+hsulli5@users.noreply.github.com>
2018-02-27 10:56:38 -0600

    Code 5

--- js/code-assignment-05.js
+++ js/code-assignment-05.js
@@ -1,4 +1,6 @@
-let projects = [{
+let projectData = []
+
+projectData[0] = {
   id: 1,
   url: "http://arcg.is/2FbRnLQ",
   title: 'Map of Austin, TX',
@@ -6,15 +8,17 @@ let projects = [{
   thumb: true,
   projectKeywords: ['Austin', 'story', 'map']
 },
-{
+
+
+projectData[1] = {
   id: 2,
   url: 'http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d',
   title: 'Earthquakes',
   desc: 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. Info pulled from USGS.',
-  thumb: true,
+  thumb: false,
   projectKeywords: ['USGS', 'Earthquakes']
-}]
+}
 
-for (let i=0; i < projects.length; i++) {
-  console.log('The value of i is; ' +i)
+for (let i=0; i < projectData.length; i++) {
+  console.log('image/ss-project' + projectData[i].id + '.png' )
 }
