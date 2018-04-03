cd181935d669cdf6b547f5cd7543ba0dd517e8fc
hsulli5 <35343448+hsulli5@users.noreply.github.com>
2018-04-03 11:50:52 -0500

    webmap3

---
+++ map/webmap-assignment-03.js
@@ -0,0 +1,24 @@
+let mymap = L.map('webmap3').setView([39.295431, -76.61124], 13)
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png').addTo(mymap)
+
+let redIcon = L.icon({
+   iconUrl: 'leaf-red.png',
+   shadowUrl: 'leaf-shadow.png',
+   iconSize: [38, 95], // size of the icon
+   shadowSize: [50, 64], // size of the shadow
+   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+   shadowAnchor: [4, 62],  // the same for the shadow
+   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+ })
+
+let markerCoords = [39.295431, -76.61124]
+let markerOptions = {icon: redIcon}
+
+let marker = L.marker(markerCoords, markerOptions).addTo(mymap)
+
+let portcoords = [
+  [39.32, -76.6056741],
+  [39.26, -76.58324],
+  [39.22, -76.6097514]
+].addTo(mymap);
--- map/webmap-assignment01.js
+++ map/webmap-assignment01.js
@@ -3,14 +3,22 @@ let basemap = 'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'
 L.tileLayer(basemap).addTo(mymap)
 let marker = L.marker([39.295431, -76.61124]).addTo(mymap)
 let polygon = L.polygon([
-  [39.30, -76.62],
-  [39.24, -76.45],
-  [39.45, -76.78]
+  [39.32, -76.6056741],
+  [39.26, -76.58324],
+  [39.22, -76.6097514]
 ]).addTo(mymap);
 
-myshape.bindPopup('St. Katharine\'s and Wapping')
-mypoint.bindPopup('London Bridge Station')
+myshape.bindPopup('The Washington Monument')
+mypoint.bindPopup('Port of Baltimore')
 
 mymap.on('click', function (event) {
-  console.log('You clicked the map at ' + event.latlng)
+  console.log('You clicked the map at Baltimore' + event.latlng)
 })
+
+
+var latlngs = [
+    [39.2845, 76.6016],
+    [39.275322, -76.588181],
+    [39.2841, -76.6215]
+];
+var polyline = L.polyline(latlngs, {color: 'purple'}).addTo(mymap);
